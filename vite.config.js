d1948bfdfa85e543629aa0444fd0e1680ab95e40
WoodArtDesign <woodart-design@ya.ru>
2026-08-27 11:02:03 +0300

    fix: обновлён vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // ← ЭТА СТРОКА ОБЯЗАТЕЛЬНА
 })
-   // Я СОХРАНИЛ ЭТОТ ФАЙЛ
